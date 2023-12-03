fb37e160eb1c1ef9c1d926d0b44632ad096cd22f
Jared Allard <jaredallard@users.noreply.github.com>
2023-12-03 11:28:49 -0800

    day03: prettier part 1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -5,10 +5,30 @@ use std::{fs, vec};
 // Wrong: 493642
 // Wrong: 518514
 
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Symbol {
+    // pos is a representation of the location of the symbol as it would
+    // appear on a 2d array ()
+    pos: Vec<usize>,
+}
+
 // is_special_char returns true if the provided character is not a digit
 // or a '.'.
-fn is_special_char(c: char) -> bool {
-    !c.is_digit(10) && c != '.'
+fn is_special_char(c: &char) -> bool {
+    !c.is_digit(10) && *c != '.'
+}
+
+// process_edges returns all symbols found within the provided edges.
+fn has_special_char_edge(edges: Vec<Option<&char>>) -> bool {
+    for e in edges.iter() {
+        let c = e.unwrap_or(&'.');
+        if is_special_char(c) {
+            return true;
+        }
+    }
+
+    // didn't find one, v sad.
+    return false;
 }
 
 fn main() {
@@ -32,90 +52,71 @@ fn main() {
     for y in 0..graph.len() {
         let row = graph.get(y).expect("should succeed");
 
-        let mut numbers: Vec<char> = vec![];
+        let mut numbers: Vec<usize> = vec![];
         let mut numbers_has_symbol = false;
 
+        // The horrible == 0 checks are because we can't look for
+        // -1. I'm sorry. I'm so bad at this.
         for x in 0..row.len() {
             let cur = row.get(x).expect("failed to get self");
 
-            let mut up = '.';
-            let mut up_left = '.';
-            let mut up_right = '.';
-            let mut down = '.';
-            let mut down_left = '.';
-            let mut down_right = '.';
-            let mut left = '.';
-            let mut right = '.';
+            let default_row: &Vec<char> = &vec![];
+            let up_row = graph
+                .get(if y == 0 { graph.len() } else { y - 1 })
+                .unwrap_or(default_row);
+            let down_row = graph.get(y + 1).unwrap_or(default_row);
 
-            if y != 0 {
-                let up_row = graph.get(y - 1).expect("failed to get above row");
-                up = *up_row.get(x).expect("should get self");
-
-                if x != row.len() - 1 {
-                    up_right = *up_row.get(x + 1).expect("failed to get up right ");
-                }
-                if x != 0 {
-                    up_left = *up_row.get(x - 1).expect("failed to get up left");
-                }
-            }
-            if y != graph.len() - 1 {
-                let down_row = graph.get(y + 1).expect("failed to get down row");
-                down = *down_row.get(x).expect("should get self");
-
-                if x != row.len() - 1 {
-                    down_right = *down_row.get(x + 1).expect("failed to get down right");
-                }
-                if x != 0 {
-                    down_left = *down_row.get(x - 1).expect("failed to get down left");
-                }
-            }
-
-            if x != 0 {
-                left = *row.get(x - 1).expect("should get left");
-            }
-            if x != row.len() - 1 {
-                right = *row.get(x + 1).expect("can't get right")
-            }
+            let edges = vec![
+                up_row.get(if x == 0 { up_row.len() } else { x - 1 }), // up left
+                up_row.get(x),                                         // up
+                up_row.get(x + 1),                                     // up right
+                row.get(x + 1),                                        // right
+                down_row.get(x + 1),                                   // down right
+                down_row.get(x),                                       // down
+                down_row.get(if x == 0 { down_row.len() } else { x - 1 }), // down left
+                row.get(if x == 0 { row.len() } else { x - 1 }),       // left
+            ];
 
             // Not a digit, and we have numbers, we should save it.
             let is_digit = cur.is_digit(10);
 
             // If we have a digit, track it.
             if is_digit {
-                numbers.push(*cur);
+                numbers.push(
+                    cur.to_digit(10)
+                        .expect("failed to parse number as a number") as usize,
+                );
 
-                // has a symbol, then mark we had one.
-                if is_special_char(up)
-                    || is_special_char(down)
-                    || is_special_char(left)
-                    || is_special_char(right)
-                    || is_special_char(up_left)
-                    || is_special_char(up_right)
-                    || is_special_char(down_left)
-                    || is_special_char(down_right)
-                {
-                    numbers_has_symbol = true
+                // Already found one, don't need to process again.
+                if !numbers_has_symbol {
+                    if has_special_char_edge(edges) {
+                        numbers_has_symbol = true;
+                    }
                 }
             }
 
+            // If we're not a digit, or if we're at the end of the row,
+            // close out the current number (if there is one w/ a
+            // symbol) or toss and reset.
             if !is_digit || x == row.len() - 1 {
                 // Otherwise, we're either ending a numbers vec or we're
                 // skipping it.
                 if !numbers.is_empty() && numbers_has_symbol {
-                    let number = numbers.iter().fold(0, |acc, &num| {
-                        acc * 10 + num.to_digit(10).expect("failed to parse char as number")
-                    });
+                    let number = numbers.iter().fold(0, |acc, &num| acc * 10 + num);
 
                     println!("Commit {:?} -> {} (S{})", numbers, number, sum);
                     sum += number;
                 }
 
-                // reset
+                // find a new number.
                 numbers = vec![];
-                numbers_has_symbol = false
+                numbers_has_symbol = false;
+
+                continue;
             }
         }
     }
 
+    // Correct: 539433
     println!("Problem 1: {}", sum)
 }
